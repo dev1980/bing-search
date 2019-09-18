310ce7ce995a7278a2df561b6777fece63796123
Miguel Prada <mapra99@gmail.com>
2019-09-18 14:42:26 -0500

    Added Rest Client code

--- bing-search.rb
+++ bing-search.rb
@@ -1,9 +1,20 @@
 require 'rest-client'
-a = RestClient.get 'http://bing.com'
-a.code
-200
-a.body
-"Display code of the webpage body"
+# a = RestClient.get 'http://bing.com'
+# a.code
+# 200
+# a.body
+# "Display code of the webpage body"
+#
+# a.cookies
+# => {"MUID"=>"01794D2E6C2E6AA12ED940F56DC26B9E", "MUIDB"=>"01794D2E6C2E6AA12ED940F56DC26B9E", "SRCHD"=>"AF=NOFORM", "SRCHUID"=>"V=2&GUID=E8B2D525002C4520AFBDE23905AEB8BE&dmnchg=1", "SRCHUSR"=>"DOB=20190918", "_EDGE_S"=>"F=1&SID=1A101321E1EA64571B701EFAE006654C", "_EDGE_V"=>"1", "_SS"=>"SID=1A101321E1EA64571B701EFAE006654C"}
 
-a.cookies
-=> {"MUID"=>"01794D2E6C2E6AA12ED940F56DC26B9E", "MUIDB"=>"01794D2E6C2E6AA12ED940F56DC26B9E", "SRCHD"=>"AF=NOFORM", "SRCHUID"=>"V=2&GUID=E8B2D525002C4520AFBDE23905AEB8BE&dmnchg=1", "SRCHUSR"=>"DOB=20190918", "_EDGE_S"=>"F=1&SID=1A101321E1EA64571B701EFAE006654C", "_EDGE_V"=>"1", "_SS"=>"SID=1A101321E1EA64571B701EFAE006654C"}
+first_part = "https://www.bing.com/search?q"
+s = gets.chomp
+url = first_part + s
+a = RestClient.get url
+puts "Search OK" if a.code==200
+puts a.cookies
+puts a.headers
+puts a.body
+
+File.write('result.html', a.body)
